cd2d0b871d0f3c01fea271f7a81b26b9b7efb785
nabinoy <nabinoybaroi007@gmail.com>
2023-04-14 14:53:09 +0530

    wishlsit page added

--- lib/screens/home_pages/wishlist.dart
+++ lib/screens/home_pages/wishlist.dart
@@ -18,7 +18,7 @@ class Wishlist extends StatefulWidget {
 class _WishlistState extends State<Wishlist> {
   @override
   Widget build(BuildContext context) {
-    List<Places2> pd=[];
+    List<Places2> pd = [];
     return Scaffold(
       body: FutureBuilder(
         future: APIService.placeAll().then((value) => {pd.addAll(value)}),
@@ -26,6 +26,18 @@ class _WishlistState extends State<Wishlist> {
           if (snapshot.connectionState == ConnectionState.done) {
             return Column(
               children: [
+                Row(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.only(left: 16),
+                      child: const Text(
+                        'Your wishlist',
+                        style: TextStyle(
+                            fontSize: 24, fontWeight: FontWeight.w600),
+                      ),
+                    ),
+                  ],
+                ),
                 Expanded(
                   child: ListView.builder(
                     shrinkWrap: true,
@@ -34,52 +46,120 @@ class _WishlistState extends State<Wishlist> {
                     // padding: const EdgeInsets.fromLTRB(5, 5, 5, 5),
                     itemCount: placeCount,
                     itemBuilder: (BuildContext context, int index) {
-                      return GestureDetector(
-                        onTap: () {
-                          Navigator.pushNamed(context, Place.routeName,
-                              arguments: [pd[index]]);
-                        },
-                        child: Column(
+                      return Container(
+                        margin: const EdgeInsets.symmetric(
+                            vertical: 8.0, horizontal: 10),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20.0),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.grey.withOpacity(0.3),
+                              spreadRadius: 2.0,
+                              blurRadius: 5.0,
+                              offset: const Offset(0, 3),
+                            ),
+                          ],
+                          color: Colors.white,
+                        ),
+                        width: double.infinity,
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
                             Container(
-                              width: double.infinity,
-                              decoration: BoxDecoration(
-                                color: Colors.white,
+                              margin: const EdgeInsets.all(12),
+                              child: ClipRRect(
                                 borderRadius: BorderRadius.circular(10.0),
-                              ),
-                              child: Column(
-                                children: [
-                                  Container(
-                                    margin: const EdgeInsets.all(12),
-                                    child: ClipRRect(
-                                      borderRadius: BorderRadius.circular(10.0),
-                                      child: CachedNetworkImage(
-                                        height: 200,
-                                        width:
-                                            MediaQuery.of(context).size.width,
-                                        imageUrl: pd[index].images.first
-                                            .secureUrl,
-                                        placeholder: (context, url) =>
-                                            Image.memory(
-                                          kTransparentImage,
-                                          fit: BoxFit.cover,
-                                        ),
-                                        fadeInDuration:
-                                            const Duration(milliseconds: 200),
-                                        fit: BoxFit.cover,
-                                      ),
-                                    ),
+                                child: CachedNetworkImage(
+                                  height:
+                                      MediaQuery.of(context).size.width * .25,
+                                  width:
+                                      MediaQuery.of(context).size.width * .25,
+                                  imageUrl: pd[index].images.first.secureUrl,
+                                  placeholder: (context, url) => Image.memory(
+                                    kTransparentImage,
+                                    fit: BoxFit.cover,
                                   ),
-                                  Text(
+                                  fadeInDuration:
+                                      const Duration(milliseconds: 200),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              width: 6,
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const SizedBox(
+                                  height: 15,
+                                ),
+                                SizedBox(
+                                  width:
+                                      MediaQuery.of(context).size.width * .55,
+                                  child: Text(
                                     pd[index].name,
                                     style: const TextStyle(
                                         fontSize: 16,
                                         fontWeight: FontWeight.w600),
                                   ),
-                                  Text(pd[index].address.city,
-                                      style: const TextStyle(fontSize: 12)),
-                                ],
-                              ),
+                                ),
+                                Text(pd[index].address.city,
+                                    style: const TextStyle(fontSize: 12)),
+                                Row(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceEvenly,
+                                  children: [
+                                    MaterialButton(
+                                      elevation: 0,
+                                      onPressed: () {
+                                        Navigator.pushNamed(
+                                            context, Place.routeName,
+                                            arguments: [pd[index]]);
+                                      },
+                                      color: Colors.green,
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(50)),
+                                      child: Row(
+                                        children: const [
+                                          Text(
+                                            'Visit',
+                                            style: TextStyle(
+                                                color: Colors.white,
+                                                fontWeight: FontWeight.w600),
+                                          ),
+                                          SizedBox(
+                                            width: 4,
+                                          ),
+                                          Icon(
+                                            Icons.arrow_forward_sharp,
+                                            size: 16,
+                                            color: Colors.white,
+                                          )
+                                        ],
+                                      ),
+                                    ),
+                                    const SizedBox(
+                                      width: 10,
+                                    ),
+                                    MaterialButton(
+                                      elevation: 0,
+                                      onPressed: () {},
+                                      color: Colors.red[400],
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(50)),
+                                      child: const Text(
+                                        'Delete',
+                                        style: TextStyle(
+                                            color: Colors.white,
+                                            fontWeight: FontWeight.w600),
+                                      ),
+                                    ),
+                                  ],
+                                )
+                              ],
                             ),
                           ],
                         ),
