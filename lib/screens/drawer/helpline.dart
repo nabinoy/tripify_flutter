e213f676a1be36ef57d128b77d0398b2b494ff7c
nabinoy <nabinoybaroi007@gmail.com>
2023-04-16 21:10:35 +0530

    helpline added

--- lib/screens/drawer/helpline.dart
+++ lib/screens/drawer/helpline.dart
@@ -5,6 +5,20 @@ import 'package:flutter/services.dart';
 import 'package:tripify/constants/global_variables.dart';
 import 'package:url_launcher/url_launcher.dart';
 
+class HelplineModel {
+  final String name;
+  final String phone;
+
+  HelplineModel({required this.name, required this.phone});
+
+  factory HelplineModel.fromJson(Map<String, dynamic> json) {
+    return HelplineModel(
+      name: json['name'],
+      phone: json['price'],
+    );
+  }
+}
+
 class Helpline extends StatefulWidget {
   static const String routeName = '/helpline';
   const Helpline({super.key});
@@ -14,16 +28,6 @@ class Helpline extends StatefulWidget {
 }
 
 class _HelplineState extends State<Helpline> {
-
-  late var data;
-
-  Future jsonInit() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  String jsonString = await rootBundle.loadString('assets/json_data/helpline.json');
-  data = jsonDecode(jsonString);
-  print(data);
-}
-
   Future<void> _makePhoneCall(String phoneNumber) async {
     final Uri launchUri = Uri(
       scheme: 'tel',
@@ -33,50 +37,62 @@ class _HelplineState extends State<Helpline> {
   }
 
   @override
-  void initState() {
-    super.initState();
-    jsonInit();
-  }
-
-  @override
   Widget build(BuildContext context) {
     SystemChrome.setPreferredOrientations([
       DeviceOrientation.portraitUp,
     ]);
     return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        backgroundColor: bgColor,
-        leading: IconButton(
-          onPressed: () async {
-            HapticFeedback.mediumImpact();
-            Navigator.pop(context);
-          },
-          icon: const Icon(
-            Icons.arrow_back_ios,
-            size: 20,
-            color: Colors.black,
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: bgColor,
+          leading: IconButton(
+            onPressed: () async {
+              HapticFeedback.mediumImpact();
+              Navigator.pop(context);
+            },
+            icon: const Icon(
+              Icons.arrow_back_ios,
+              size: 20,
+              color: Colors.black,
+            ),
           ),
         ),
-      ),
-      resizeToAvoidBottomInset: false,
-      body: ListView.builder(
-        itemCount: data.length,
-                  itemBuilder: (context, index) {
-                    return ListTile(title: data["numbers"][index]["name"],);
-                  }
-      )
-      // SingleChildScrollView(
-      //   child: Row(children: [
-      //     const Text('Call here'),
-      //     ElevatedButton(
-      //       onPressed: () {
-      //         _makePhoneCall('+919531839056');
-      //       },
-      //       child: const Text('Call'),
-      //     )
-      //   ]),
-      // ),
-    );
+        resizeToAvoidBottomInset: false,
+        body: FutureBuilder(
+          future: DefaultAssetBundle.of(context)
+              .loadString('assets/json_data/helpline.json'),
+          builder: (context, snapshot) {
+            List<HelplineModel> helpline = [];
+            if (snapshot.hasData) {
+              var json = jsonDecode(snapshot.data!);
+              json = json['numbers'];
+
+              helpline = List<HelplineModel>.from(
+                  json.map((item) => HelplineModel.fromJson(item)));
+            }
+
+            return ListView.builder(
+              itemCount: helpline.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(helpline[index].name),
+                  //subtitle: Text('\$${helpline[index].price.toStringAsFixed(2)}'),
+                );
+              },
+            );
+          },
+        )
+        // SingleChildScrollView(
+        //   child: Row(children: [
+        //     const Text('Call here'),
+        //     ElevatedButton(
+        //       onPressed: () {
+        //         _makePhoneCall('+919531839056');
+        //       },
+        //       child: const Text('Call'),
+        //     )
+        //   ]),
+        // ),
+        );
   }
 }
